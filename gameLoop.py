adac5a3ac0040d25584db68fc809aa64978f5604
Damyon <damyonolson1@gmail.com>
2023-02-20 15:20:01 -0500

    Ball :(

--- gameLoop.py
+++ gameLoop.py
@@ -3,10 +3,10 @@ import view
 import entities
 import controller
 
-clock = pygame.time.Clock()
 
-ball = entities.Ball([255, 255, 255], 10, 10)
 def game_loop(surface):
+    newBall = entities.Ball()
+    clock = pygame.time.Clock()
     while True:
 
         for event in pygame.event.get():
@@ -20,26 +20,26 @@ def game_loop(surface):
 
         entities.player1.clamp_ip(surface.get_rect())
         entities.player2.clamp_ip(surface.get_rect())
-        ball.clamp_ip(surface.get_rect())
 
-        if entities.ball.x>=790:
-            entities.ballVelocity[0] = -entities.ballVelocity[0]
-        if entities.ball.x<=0:
-            entities.ballVelocity[0] = -entities.ballVelocity[0]
-        if entities.ball.y>=590:
-            entities.ballVelocity[1] = -entities.ballVelocity[1]
-        if entities.ball.y<=0:
-            entities.ballVelocity[1] = -entities.ballVelocity[1] 
+        newBall.clamp_ip(surface.get_rect())
 
-        if entities.ball.collidedict(entities.player1) or entities.ball.collidedict(entities.player2):
-            entities.bounceBall()
+        if newBall.x>=790:
+            newBall.ballVelocity[0] = -newBall.ballVelocity[0]
+        if newBall.x<=0:
+            newBall.ballVelocity[0] = -newBall.ballVelocity[0]
+        if newBall.y>=590:
+            newBall.ballVelocity[1] = -newBall.ballVelocity[1]
+        if newBall.y<=0:
+            newBall.ballVelocity[1] = -newBall.ballVelocity[1]
 
         pygame.draw.rect(surface, entities.p1Color, entities.player1)
         pygame.draw.rect(surface, entities.p2Color, entities.player2)
-        pygame.draw.rect(surface, entities.ballColor, entities.ball)
-
-        entities.updateBall()
+        newBall.drawBall(surface)
+        
+        newBall.updateBall()
         pygame.display.update()
+
+        print(newBall.x + newBall.y)
 
         clock.tick(60)
 
